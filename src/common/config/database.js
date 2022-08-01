a57b3a54c92e36559b350ae0c085c92fa7655fbb
chenzhiwei <chenzhiwei@taetea.com.cn>
2022-08-01 16:21:28 +0800

    修改了正式环境的数据库用户名

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -5,11 +5,12 @@ module.exports = {
   database: 'nideshop',
   prefix: 'nideshop_',
   encoding: 'utf8mb4',
-  host: 'localhost', //正式
-  // host: '47.100.21.119', // 测试
+  // host: '127.0.0.1', //正式
+  host: '47.100.21.119', // 测试
   port: '3306',
-  user: 'root',
-  password: 'Zhejiaju1+',
-
+  // user: 'root',
+  // password: 'Zhejiaju1+',
+  user: 'zhejia',
+  password: 'Tw2Rq7yIF5dYqdi1Bhe%NaAWqR9yNF6d',
   dateStrings: true
 };
